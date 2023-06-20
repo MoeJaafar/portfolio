062171a9ec0aebf95dc3fc281b6335d2c31edda3
Mohammad Jaafar <m.jaafar@innopolis.university>
2023-06-20 17:19:11 +0300

    finished comic page

--- assets/js/fetchComic.js
+++ assets/js/fetchComic.js
@@ -1,5 +1,6 @@
 async function fetchComic() {
     const email = "m.jaafar@innopolis.university";
+    const params = new URLSearchParams({ email: email });
     let response = await fetch(`https://fwd.innopolis.university/api/hw2?${params.toString()}`);
     const comicId = await response.json();
     response = await fetch(`https://fwd.innopolis.university/api/comic?id=${comicId}`);
@@ -11,20 +12,27 @@ function displayComic(comicData) {
     const container = document.querySelector("#comic-container");
     container.textContent = ''; // clear previous data
 
+    const title = document.createElement("h1");
+    title.textContent = comicData.safe_title;
+    title.className = 'comic-title';
+
     const img = document.createElement("img");
     img.src = comicData.img;
     img.alt = comicData.alt;
-
-    const title = document.createElement("h2");
-    title.textContent = comicData.title;
+    img.className = 'comic-image';
 
     const date = new Date(comicData.year, comicData.month - 1, comicData.day);
-    const dateElement = document.createElement("p");
+    const dateElement = document.createElement("h2");
     dateElement.textContent = date.toLocaleDateString();
+    dateElement.className = 'comic-date';
+
 
     container.appendChild(img);
     container.appendChild(title);
     container.appendChild(dateElement);
 }
+
+
+
 
 fetchComic();
